25520fb03ec5a354e4d9261a341c8102f7fa2060
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-10-26 11:02:05 -0300

    feat: developing a strategy interface

---
+++ src/strategy/SortInterface.java
@@ -0,0 +1,13 @@
+package strategy;
+
+import java.util.List;
+
+interface SortInterface {
+    public void add(Integer Value);
+
+    public void sort();
+
+    public List<Integer> getValuesList();
+
+
+}
