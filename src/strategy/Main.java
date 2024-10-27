162c9d70d4007257d097b8458c75d9de2a55fa4f
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-10-27 11:20:27 -0300

    feat: manual tests of manual sort(er)

--- src/strategy/Main.java
+++ src/strategy/Main.java
@@ -2,12 +2,24 @@ package strategy;
 
 public class Main {
     public static void main(String[] args) {
-        SortInterface sorter = new CollectionsSort();
-        sorter.add(9);
-        sorter.add(5);
-        sorter.add(7);
-        System.out.println(sorter.getValuesList());
-        sorter.sort();
-        System.out.println(sorter.getValuesList());
+        System.out.println("CollectionSort:");
+        SortInterface sorter1 = new CollectionsSort();
+        sorter1.add(9);
+        sorter1.add(5);
+        sorter1.add(7);
+        System.out.println("Original: " + sorter1.getValuesList());
+        sorter1.sort();
+        System.out.println("Sorted: " + sorter1.getValuesList());
+
+        System.out.println("----------------------------");
+
+        System.out.println("ManualSort:");
+        SortInterface sorter2 = new ManualSort();
+        sorter2.add(9);
+        sorter2.add(5);
+        sorter2.add(7);
+        System.out.println("Original: " + sorter2.getValuesList());
+        sorter2.sort();
+        System.out.println("Sorted: " + sorter2.getValuesList());
     }
 }
